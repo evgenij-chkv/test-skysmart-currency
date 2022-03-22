9013ae644672ca60a60368895c92e2d253f9356b
evgenij-chkv <102173854+evgenij-chkv@users.noreply.github.com>
2022-03-22 23:50:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -50,12 +50,12 @@ def parse(currency, html_text):
 
 
 def get_currency(currency):
-   headers = {
-       'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11','Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-       'Accept-Encoding': 'none',
-       'Accept-Language': 'en-US,en;q=0.8',
-       'Connection': 'keep-alive'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11','Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+        'Accept-Encoding': 'none',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Connection': 'keep-alive'
     }
     r = requests.get('https://www.cbr.ru/currency_base/daily/', headers=headers)
     soup = BeautifulSoup(r.text, 'html.parser')
