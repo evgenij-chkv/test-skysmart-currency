1664df69c5c5004376118f558a9d7a3b24f16e12
evgenij-chkv <102173854+evgenij-chkv@users.noreply.github.com>
2022-03-22 23:38:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-# Done! Congratulations on your new bot. You will find it at t.me/skysmart_currency_bot. You can now add a description, about section and profile picture for your bot, see /help for a list of commands. By the way, when you've finished creating your cool bot, ping our Bot Support if you want a better username for it. Just make sure the bot is fully operational before you do this.
-#
-# Use this token to access the HTTP API:
-# 5206778605:AAF1G-dYOWhNYVUhCHM8g9GpYVQp3yo2ePc
-# Keep your token secure and store it safely, it can be used by anyone to control your bot.
-#
-# For a description of the Bot API, see this page: https://core.telegram.org/bots/api
 import os
 import telebot
 import requests
@@ -59,7 +52,6 @@ def parse(currency, html_text):
 def get_currency(currency):
     r = requests.get('https://www.cbr.ru/currency_base/daily/')
     soup = BeautifulSoup(r.text, 'html.parser')
-    print(r, r.text)
     button = soup.find('button', {'class': 'datepicker-filter_button'}) 
     button_text = ''
     if button:
